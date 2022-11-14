1ec284839893e3068f4b89cfd74e32476ead44fb
CarmenBolovan20 <92154059+CarmenBolovan20@users.noreply.github.com>
2022-11-14 18:49:14 +0000

    app
    The real Relationship Manager App.

---
+++ Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RM_APP
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
